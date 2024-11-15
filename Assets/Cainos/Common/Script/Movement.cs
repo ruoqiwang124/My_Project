33bcea5a6b48b5a69da310bfd1c848ea858e249d
ruoqiwang124 <116595181+ruoqiwang124@users.noreply.github.com>
2024-11-14 19:16:19 -0600

    update jump but failed
    update jump but failed

--- Assets/Cainos/Common/Script/Movement.cs
+++ Assets/Cainos/Common/Script/Movement.cs
@@ -4,30 +4,49 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f; 
+    // Property usage example
+    public float moveSpeed { get; set; } = 5f;
+
     public float jumpForce = 10f;
     public Transform groundCheck;
     public LayerMask groundLayer;
-    // Start is called before the first frame update
 
     private Rigidbody2D rb;
     private bool isGrounded;
+
+    // Static variable demonstration
+    private static int instanceCount = 0;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        // Increment static field
+        instanceCount++;
+        Debug.Log("Instance count: " + instanceCount);
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        // Check if grounded using Physics2D.OverlapCircle
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
-        if (Input.GetButtonDown("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, 0);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
+
+    // Event usage example
+    // This event is triggered when the player jumps
+    public event System.Action OnJump;
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        OnJump?.Invoke(); // Invoke the OnJump event
     }
 }
